bec3586cfcc415400398e86e9096a65016d39cb6
MengyaoHu <mengyao.hu@stonybrook.edu>
2022-09-06 12:17:43 -0400

    add flask file and instructions

---
+++ flask/flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Local host mian page
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+# Second web page created
+@app.route('/careers')
+def careers():
+    return 'Welcome to the career page!'
+
+# Third web page created
+@app.route('/dashboard')
+def dashboard():
+    return 'You are reaching the dashboard!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=80)
